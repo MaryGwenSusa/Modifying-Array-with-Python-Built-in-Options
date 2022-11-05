377e79a570cade7dfea145cdb7fb293db1394b2d
Mary Gwen Susa <Mary Gwen G. Susa>
2022-11-05 16:59:10 +0800

    user input and menu options

--- modifying-array-with-python-built-in-options.py
+++ modifying-array-with-python-built-in-options.py
@@ -4,6 +4,20 @@ def header():
     print(title)
     print("*" * len(title))
 
+def modifyArray():
+    """\x1b[35mMenu:\x1b[00m 
+            \x1b[32m\x1b[1m1\x1b[00m -> \x1b[4m\x1b[36mAdd a number.\x1b[00m 
+            \x1b[32m\x1b[1m2\x1b[00m -> \x1b[4m\x1b[36mInsert a number.\x1b[00m 
+            \x1b[32m\x1b[1m3\x1b[00m -> \x1b[4m\x1b[36mModify a number.\x1b[00m
+            \x1b[32m\x1b[1m4\x1b[00m -> \x1b[4m\x1b[36mDelete a number.\x1b[00m 
+            \x1b[32m\x1b[1m5\x1b[00m -> \x1b[4m\x1b[36mArrange in ascending order.\x1b[00m  
+            \x1b[32m\x1b[1m6\x1b[00m -> \x1b[4m\x1b[36mArrange in descending order.\x1b[00m 
+    """
+    try:
+        askUser = int(input('\x1b[31mWhat do you want to do (Enter 1-6)?\x1b[00m \n >\x1b[34m '))
+    except ValueError:
+        print('\x1b[00m\x1b[32mThe entered option is not among the choices.\x1b[00m')
+
 
 
 
